eb17c81bd602d336c8e33ebb3a0eb4b374ba8fe4
Alexia <cismaru.mariaalexia@student.kdg.be>
2024-03-04 10:01:50 +0100

    Display datasets

--- Chatbot/streamlit_app.py
+++ Chatbot/streamlit_app.py
@@ -37,9 +37,6 @@ if 'displayed_df' not in st.session_state:
 if st.session_state.displayed_df is not None:
     st.dataframe(st.session_state.displayed_df)
 
-# # Regex pattern for UUID
-# uuid_pattern = re.compile(r'\b[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\b')
-
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
@@ -47,77 +44,11 @@ if prompt := st.chat_input("What is up?"):
 
     # Display user input in chat message container
     with st.chat_message("user"):
-        st.markdown(prompt)  # Display the user's input
+        st.markdown(prompt)  # Display the user's input 
 
-    # Initialize dataset_id as None
-    dataset_id = None
+    response = chatbot.get_gpt3_response(prompt)
 
-    # # Attempt to extract a UUID from the prompt
-    # match = uuid_pattern.search(prompt)
-    # if match:
-    #     dataset_id = match.group()
-
-    #### DATAFRAMES ####
-    if dataset_id and ("show" in prompt or "print" in prompt or "display" in prompt or "fetch" in prompt):
-        # Fetch the dataset contents using the extracted dataset_id
-        df = DatasetManager.get_datasets_by_dataset_id(dataset_id)
-        st.session_state.displayed_df = df  # Store the data frame in the session state
-        response = st.dataframe(df)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-    else:
-        # Get the chatbot response for prompts without a dataset request or without a UUID
-        response = chatbot.get_gpt3_response(prompt)
-
-    # #### GRAPHS ####
-    # if 'graph' in response or 'plot' in response or 'visual' in response or 'chart' in response:
-    #     df = DatasetManager.print_dataset_contents(dataset_id)
-
-    #     plot_type = None
-    #     match = re.search(r'plot\s+(\w+)\s+x=(\w+)\s+y=(\w+)', response)
-    #     if match:
-    #         plot_type = match.group(1)
-    #         x_column = match.group(2)
-    #         y_column = match.group(3)
-
-    #     # Generate and display the plot based on the plot type
-    #     if plot_type == 'line':
-    #         plt.plot(df)
-    #         plt.xlabel('X-axis')
-    #         plt.ylabel('Y-axis')
-    #         plt.title('Line Plot')
-    #         st.pyplot()
-    #     elif plot_type == 'bar':
-    #         plt.bar(df.line, df['value'])
-    #         plt.xlabel('X-axis')
-    #         plt.ylabel('Y-axis')
-    #         plt.title('Bar Plot')
-    #         st.pyplot()
-    #     elif plot_type == 'scatter':
-    #         plt.scatter(df['x'], df['y'])
-    #         plt.xlabel('X-axis')
-    #         plt.ylabel('Y-axis')
-    #         plt.title('Scatter Plot')
-    #         st.pyplot()
-    #     elif plot_type == 'hist':
-    #         plt.hist(df['value'])
-    #         plt.xlabel('X-axis')
-    #         plt.ylabel('Y-axis')
-    #         plt.title('Histogram')
-    #         st.pyplot()
-    #     elif plot_type == 'box':
-    #         plt.boxplot(df['value'])
-    #         plt.xlabel('X-axis')
-    #         plt.ylabel('Y-axis')
-    #         plt.title('Box Plot')
-    #         st.pyplot()
-    #     # elif plot_type == 'pie':
-    #     #     fig = px.pie(df, values='value', names='category', title='Pie Chart')
-    #     #     st.plotly_chart(fig)
-    # else:
-    #     # Get the chatbot response for prompts without a dataset request or without a UUID
-    #     response = chatbot.get_gpt3_response(prompt)
-
-        # Display assistant response in chat message container and add to chat history
+    # Display assistant response in chat message container and add to chat history
     with st.chat_message("assistant"):
         st.markdown(response)  # Display the chatbot response
     st.session_state.messages.append({"role": "assistant", "content": response})
