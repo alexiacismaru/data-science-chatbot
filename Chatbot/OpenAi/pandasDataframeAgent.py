0e455c32303fcafd7e0672234ba67520858ae1f9
Alexia <cismaru.mariaalexia@student.kdg.be>
2024-03-04 11:27:38 +0100

    Logging

--- Chatbot/OpenAi/pandasDataframeAgent.py
+++ Chatbot/OpenAi/pandasDataframeAgent.py
@@ -10,7 +10,8 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(leve
 logger = logging.getLogger(__name__)
 
 def process_dataframe_with_natural_language(df: pd.DataFrame, query):
-    try: 
+    print(f"Type of df: {type(df)}") 
+    try:
         agent = create_pandas_dataframe_agent(
             ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo-0125"),
             df,
