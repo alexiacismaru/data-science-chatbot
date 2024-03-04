f8bb3376b1d2555451b2bfca192916f747f9a264
Alexia <cismaru.mariaalexia@student.kdg.be>
2024-03-04 11:25:01 +0100

    Logging

--- Chatbot/OpenAi/pandasDataframeAgent.py
+++ Chatbot/OpenAi/pandasDataframeAgent.py
@@ -3,20 +3,27 @@ from dotenv import load_dotenv
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
 from langchain.agents.agent_types import AgentType 
-from Data.dataset_manager import DatasetManager
+import streamlit as st
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def process_dataframe_with_natural_language(df: pd.DataFrame, query):
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError(f"Expected pandas DataFrame, got {type(df)}")
-    
-    agent = create_pandas_dataframe_agent(
-        ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo-0125"),
-        df,
-        verbose=False,
-        agent_type=AgentType.OPENAI_FUNCTIONS
-    ) 
-    return agent.invoke(query)
+    try: 
+        agent = create_pandas_dataframe_agent(
+            ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo-0125"),
+            df,
+            verbose=False,
+            agent_type=AgentType.OPENAI_FUNCTIONS
+        )
+        return agent.invoke(query)
+    except ValueError as e:
+        logger.error(f"ValueError encountered: {e}")
+        st.error(f"An error occurred: {e}")  # Display an error message in the Streamlit app
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        st.error("An unexpected error occurred. Please try again.") 
 
 
 # datasets = DatasetManager.get_datasets_by_dataset_id(["078919b5-1d15-4cef-817b-3df5af7e04f2"])
