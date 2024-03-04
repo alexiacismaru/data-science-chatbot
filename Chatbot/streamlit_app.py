381beac705d799fa84cdfaef204f66922611c876
Alexia <cismaru.mariaalexia@student.kdg.be>
2024-03-04 12:46:49 +0100

    Display dataframes

--- Chatbot/streamlit_app.py
+++ Chatbot/streamlit_app.py
@@ -95,10 +95,10 @@ if prompt := st.chat_input("What is up?"):
         # Fetch the dataset contents using the extracted dataset_id
         df = DatasetManager.get_datasets_by_dataset_id(dataset_id)
         st.session_state.displayed_df = df  
-        st.dataframe(df)
-    else:
-        # Get the chatbot response for prompts without a dataset request or without a UUID
-        response = chatbot.get_gpt3_response(prompt)
+        st.dataframe(df) 
+        
+    # Get the chatbot response for prompts without a dataset request or without a UUID
+    response = chatbot.get_gpt3_response(prompt)
 
     # Display assistant response in chat message container and add to chat history
     with st.chat_message("assistant"):
