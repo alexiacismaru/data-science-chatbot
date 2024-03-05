a37305fac8ce02afa9daa06c8338eda831feef0f
Alexia <cismaru.mariaalexia@student.kdg.be>
2024-03-05 10:33:56 +0100

    Google SQL connection

--- Chatbot/streamlit_app.py
+++ Chatbot/streamlit_app.py
@@ -6,8 +6,7 @@ import os
 import matplotlib.pyplot as plt
 from OpenAi.openai_client import OpenAIClient
 from Data.dataset_manager import DatasetManager
-import re
-import Streamlit.db_connection as db_connection
+import re 
 
 # Load environment variables from .env
 load_dotenv()
@@ -19,8 +18,56 @@ api_key = os.getenv("OPENAI_API_KEY")
 chatbot = OpenAIClient(api_key=api_key)
 
 # Initialize database connection
-connection = db_connection.getconn()
+# connection = db_connection.getconn()
 
+from google.cloud.sql.connector import connector
+from dotenv import load_dotenv
+import os 
+
+# Load variables from .env file into environment
+load_dotenv()
+
+### CONNECT TO GOOGLE CLOUD SQL DATABASE ###
+instance_name = os.getenv("INSTANCE_CONNECTION_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+db = os.getenv("DB_NAME")    
+
+conn = connector.connect(
+        instance_name,  
+        'pymysql',
+        user=user,
+        password=password,
+        db=db
+    )
+
+def getconn():
+    return conn
+
+# Create a table to store feedback
+mycursor = conn.cursor()
+# mycursor.execute("CREATE DATABASE feedback")
+
+# check to see if the database was created
+# mycursor.execute("SHOW DATABASES")
+
+# for x in mycursor:
+#   print(x)
+
+# mycursor.execute("CREATE TABLE feedback (id INT AUTO_INCREMENT PRIMARY KEY, input VARCHAR(255), emoji VARCHAR(255) CHARACTER SET utf8mb4, date DATE, time TIME)")
+
+mycursor.execute("SELECT * FROM feedback")
+results = mycursor.fetchall()
+
+for row in results:
+    id = row[0]
+    input = row[1]
+    emoji = row[2]
+    date = row[3].strftime("%d-%m-%Y")
+    time = row[4]
+    print(f"({id}, {input}, {emoji}, {date}, {time})")
+
+### STREANLIT APP ###
 st.sidebar.title("The Lab - FAN app")
 
 # Initialize chat history
@@ -73,6 +120,7 @@ emoji_options = ["😀 Happy", "😐 Neutral", "😒 Dissatisfied", "😠 Angry"
 with st.sidebar:
     form_expander = st.expander("Feedback", expanded=False)
 
+connection = getconn()
 # Feedback form
 with form_expander:
     with st.form(key="feedback_form", clear_on_submit=True):
